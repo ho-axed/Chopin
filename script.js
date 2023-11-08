739cae6a616f42fc90811a6b8ba7f83370e9b723
Hoaxed <91023803+ho-axed@users.noreply.github.com>
2023-11-08 00:12:25 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const Iniciodesesion = document.querySelector('.Iniciodesesion');
-const Linklogin = document.querySelector('.Link-login');
-const Linkregistrarse = document.querySelector('.Link-registrarse');
-Linkregistrarse.addEventListener('click', ()=> {
-    Iniciodesesion.classList.add('activo')
-})
-Linklogin.addEventListener('click', ()=> {
-    Iniciodesesion.classList.remove('activo')
-})
