a006cdd587598f5dc6a70f3e48fc7b12ecbf2c2b
Hoaxed <91023803+ho-axed@users.noreply.github.com>
2023-11-08 21:34:26 -0300

    Update conexion_be.php

--- php/conexion_be.php
+++ php/conexion_be.php
@@ -1,10 +1,15 @@
 <?php
-    $conexion= mysqli_connect("localhost", "root", "", "login_register_db");
-    /*/
-    if ($conexion){
-        echo'conectado exitosamente a la base de datos';
-    }else{
-        echo 'no se ha podido conectar a la base de datos';
-    }
-    */
+session_start();
+$DB_HOST=$_ENV["DB_HOST"];
+$DB_USER=$_ENV["DB_USER"];
+$DB_PASSWORD=$_ENV["DB_PASSWORD"];
+$DB_NAME=$_ENV["DB_NAME"];
+$DB_PORT=$_ENV["DB_PORT"];
+/*/$conexion= mysqli_connect("localhost", "root", "", "login_register_db");
+if ($conexion){
+    echo'conectado exitosamente a la base de datos';
+}else{
+    echo 'no se ha podido conectar a la base de datos';
+}
+*/
 ?>
